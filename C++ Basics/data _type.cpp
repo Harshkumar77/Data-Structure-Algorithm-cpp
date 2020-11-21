fe6de9147c3221a239bda942557de1ffb0882c5f
Harshkumar77 <72347597+Harshkumar77@users.noreply.github.com>
2020-11-21 13:15:35 +0530

    Create data _type.cpp

---
+++ C++ Basics/data _type.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Data Types
+
+    //integers
+
+    int x ;
+    unsigned int x2 ;
+    long int x3;
+    long long unsigned int x4;
+
+    //double 
+    double d;
+    long long unsigned double d2;
+
+    // float
+    float f;
+    long long unsigned float f2;
+
+    //char
+    char c = 'A';
+
+    //Boolean 
+    bool b5 = true;
+    bool b6 = 0;
+
+    //string 
+    string s1 = "Are you reading this ? ";
+
+
+    return 0;
+}
