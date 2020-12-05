f054f7afcc8fc978a3164357b9a77b772e1ece2d
Your Name <you@example.com>
2020-12-05 23:38:25 +0530

    modified 2Pair.cpp

---
+++ 18VectorAndPair/2Pair.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //initialising pair
+    pair<int , string> p ;
+
+    //acsessing element
+    p.first=5;
+    p.second=5;
+      
+}
