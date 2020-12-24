41c3a95730e8ddbcf2171692a2682e6487a0857b
Your Name <you@example.com>
2020-12-25 02:39:19 +0530

    Addded code for function overloading

--- 21OOPS/4Polymorphism.cpp
+++ 21OOPS/4Polymorphism.cpp
@@ -1,8 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//function overloading
+
+class OBJ{
+    public :
+        void A()
+        {
+            cout<<"function with No arg\n";
+        }
+    public :
+        void A(int a)
+        {
+            cout<<"function with Integer arg\n";
+        }
+    public :
+        void A(double a)
+        {
+            cout<<"function with Double arg\n";
+        }
+} ;
+
 int main()
 {
-    
+    OBJ o;
+    o.A();
+    o.A(21);
+    o.A(99.32);
     return 0;
 }
