d9750526eff2a2f39d9fa3f2e204ac9407fded3f
Your Name <you@example.com>
2021-03-09 23:07:35 +0530

    added code in stack

--- 23_Stack_data_structure/04_infix_to_postfix.cpp
+++ 23_Stack_data_structure/04_infix_to_postfix.cpp
@@ -15,12 +15,41 @@ int prec(char a)
 
 string infix_to_postfix(string s)
 {
+    string postfix = "";
+    stack<char> st;
     for (int i = 0; i < s.length(); ++i)
     {
-        
+        if ((s[i] >= 'A' and s[i] <= 'Z') or (s[i] >= 'a' and s[i] <= 'z'))
+            postfix.push_back(s[i]);
+        else if (s[i] == '(')
+            st.push(s[i]);
+        else if (s[i] == ')')
+        {
+            while (st.top() != '(')
+            {
+                postfix.push_back(st.top());
+                st.pop();
+            }
+            st.pop();
+        }
+        else
+        {
+            while (!st.empty() and prec(s[i]) < prec(st.top()))
+            {
+                postfix.push_back(st.top());
+                st.pop();
+            }
+            st.push(s[i]);
+        }
     }
+    while (!st.empty())
+        postfix.push_back(st.top()), st.pop();
+    return postfix;
 }
 
 int main()
 {
+    string exp;
+    cin >> exp;
+    cout << infix_to_postfix(exp);
 }
