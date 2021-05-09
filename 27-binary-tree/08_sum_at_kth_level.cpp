2103aa2af049d522ca3c96b21e6e640191160423
mighty_volcano <kharsh110081@gmail.com>
2021-05-10 04:20:23 +0530

    sum at kth level of binary tree

--- 27-binary-tree/08_sum_at_kth_level.cpp
+++ 27-binary-tree/08_sum_at_kth_level.cpp
@@ -1,6 +1,67 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left = NULL;
+    node *right = NULL;
+
+    node(int val)
+    {
+        data = val;
+    }
+};
+int nodeSum(node *root, int level)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    int sum = 0, l = 0;
+    while (!q.empty())
+    {
+        node *a = q.front();
+        q.pop();
+        if (a == NULL and !q.empty())
+        {
+            q.push(NULL);
+            l++;
+            if (l == level)
+                return sum;
+            sum = 0;
+        }
+        else if (not q.empty())
+        {
+            sum += a->data;
+            if (a->left != NULL)
+                q.push(a->left);
+            if (a->right != NULL)
+                q.push(a->right);
+        }
+    }
+}
 int main()
 {
-    // while (true)
-    std::cout << 'i' << '\n';
+    /*
+    
+    Creating this binary tree
+        1
+       / \
+      2   3
+     / \ / \
+    4  5 6  7
+
+    */
+
+    node *root = new node(1);
+    root->left = new node(2);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right = new node(3);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    cout << nodeSum(root, 2);
 }
