54114992f28bb21d11fa01cea1844b2385e96384
mighty_volcano <kharsh110081@gmail.com>
2021-05-12 20:13:17 +0530

    Left and Right View of Binary Tree 🌴

---
+++ 27-binary-tree/16_left_view_BT.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left = NULL;
+    node *right = NULL;
+
+    node(int val)
+    {
+        data = val;
+    }
+};
+
+void leftViewOfBT(node *root)
+{
+    int n;
+    queue<node *> q;
+    q.push(root);
+    node *a;
+    while (!q.empty())
+    {
+        n = q.size();
+        for (int i = 0; i < n; i++)
+        {
+            a = q.front();
+            q.pop();
+            if (i == 0)
+                cout << a->data << ' ';
+            if (a->left)
+                q.push(a->left);
+            if (a->right)
+                q.push(a->right);
+        }
+    }
+}
+
+int main()
+{
+    /*
+    
+    Creating this binary tree
+        1
+       / \
+      2   3
+     / \ / \
+    4  5 6  7
+
+    */
+
+    node *root = new node(1);
+    root->left = new node(2);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right = new node(3);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    leftViewOfBT(root);
+}
