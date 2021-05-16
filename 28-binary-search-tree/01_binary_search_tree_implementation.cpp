4cfcade52c738b9664a058947d626682d679f525
mighty_volcano <kharsh110081@gmail.com>
2021-05-16 17:49:07 +0530

    Building BST from an array

---
+++ 28-binary-search-tree/01_binary_search_tree_implementation.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+/*
+*        Binary search tree is just normal binary tree                  
+*           1. Left node value must be less than the parent node            
+*           2. Right node value must be greater than the parent node        
+*           3. No duplicates must be there in tree                      
+*
+*        Program to build BST from an array.
+*/
+struct node
+{
+    int data;
+    node *left = NULL;
+    node *right = NULL;
+    node(int val)
+    {
+        data = val;
+    }
+    node(int val, node *a, node *b)
+    {
+        data = val;
+        left = a;
+        right = b;
+    }
+};
+
+void insertNode(node *root, int value)
+{
+    /*
+        Using properties of BST to inserting nodes
+    */
+    if (value < root->data)
+    {
+        if (root->left == NULL)
+            root->left = new node(value);
+        else
+            insertNode(root->left, value);
+    }
+    else if (root->right == NULL)
+        root->right = new node(value);
+    else
+        insertNode(root->right, value);
+}
+
+node *buildBST(vector<int> v)
+{
+    int n = v.size();
+    if (n == 0)
+        return NULL;
+    node *root = new node(v[0]);
+    for (int i = 1; i < n; i++)
+        insertNode(root, v[i]);
+    return root;
+}
+
+void printInorder(node *root)
+{
+    /*      Inorder of a BST is always *SORTED*    */
+    if (root == NULL)
+        return;
+    printInorder(root->left);
+    cout << root->data << ' ';
+    printInorder(root->right);
+}
+
+int main()
+{
+    vector<int> v = {5, 1, 4, 3, 2, 7, 6};
+    printInorder(buildBST(v));
+}
