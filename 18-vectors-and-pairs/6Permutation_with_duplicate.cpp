febe8f92c85e582affa74d0c77dab618e8904388
mighty_volcano <kharsh110081@gmail.com>
2021-05-17 21:09:58 +0530

    Enhanced code 🎇

--- 18-vectors-and-pairs/6Permutation_with_duplicate.cpp
+++ 18-vectors-and-pairs/6Permutation_with_duplicate.cpp
@@ -1,11 +1,9 @@
-#include<iostream>
-#include<vector>
 #include <bits/stdc++.h>
 using namespace std;
 
-void permute (vector<int> &num , vector<vector<int>> &big , int idx)
+void permute(vector<int> &num, vector<vector<int>> &big, int idx)
 {
-    if (idx==num.size())
+    if (idx == num.size())
     {
         big.push_back(num);
         return;
@@ -13,34 +11,34 @@ void permute (vector<int> &num , vector<vector<int>> &big , int idx)
     for (int i = idx; i < num.size(); i++)
     {
 
-        if (i!=idx and num[i]==num[i-1])
+        if (i != idx and num[i] == num[i - 1])
         {
             continue;
         }
-        swap(num[i] , num[idx]);
-        permute(num,big,idx+1);
-        swap(num[i] , num[idx]);
-    }   
+        swap(num[i], num[idx]);
+        permute(num, big, idx + 1);
+        swap(num[i], num[idx]);
+    }
 }
 
 int main()
 {
-    int n ; 
-    cin>>n;
+    int n;
+    cin >> n;
     vector<int> num(n);
     for (auto &i : num)
     {
-        cin>>i;
+        cin >> i;
     }
-    sort(num.begin(),num.end());
+    sort(num.begin(), num.end());
     vector<vector<int>> big;
-    permute(num,big,0);
+    permute(num, big, 0);
     for (auto &i : big)
     {
         for (auto &ii : i)
         {
-            cout<<ii<<" ";
+            cout << ii << " ";
         }
-        cout<<'\n';
-    }    
+        cout << '\n';
+    }
 }
