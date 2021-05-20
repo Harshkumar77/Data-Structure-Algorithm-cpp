2b0cc8d3183494153c6d7166f5a4e471c4ef630f
mighty_volcano <kharsh110081@gmail.com>
2021-05-20 22:48:24 +0530

    Enhanced code ✨

--- 28-binary-search-tree/06_balanced_BST_from_sorted_array.cpp
+++ 28-binary-search-tree/06_balanced_BST_from_sorted_array.cpp
@@ -24,12 +24,16 @@ struct node
         right = b;
     }
 };
-
+/*
+1. Finding the middle of the array
+2. Left half of the array would contribute to left subtree and \
+    right half would be right and the middle will become root node
+*/
 node *buildFromSortedArray(int inorder[], int l, int r)
 {
-    if (l > r)
+    if (l > r) // base case
         return NULL;
-    int mid = (l + r) / 2;
+    int mid = (l + r) / 2; // middle index
     node *root = new node(inorder[mid]);
     root->left = buildFromSortedArray(inorder, l, mid - 1);
     root->right = buildFromSortedArray(inorder, mid + 1, r);
@@ -46,7 +50,7 @@ void printInorder(node *root)
 
 int main()
 {
-    int inorder[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    node *root = buildFromSortedArray(inorder, 0, sizeof(inorder) / sizeof(root[0]) - 1);
+    int inorder[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    node *root = buildFromSortedArray(inorder, 0, sizeof(inorder) / 4 - 1);
     printInorder(root), cout << '\n';
 }
